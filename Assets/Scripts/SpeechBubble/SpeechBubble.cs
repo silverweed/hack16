3a443adcc70253fc4b67132e2156bf3b97703340
Federico Aliprandi <fede.alip@gmail.com>
2016-11-07 21:45:41 +0100

    Bubble Speech

---
+++ Assets/Scripts/SpeechBubble/SpeechBubble.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeechBubble : MonoBehaviour
+{
+    public Sprite[] bubbleSprites;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = bubbleSprites[Random.Range(0, bubbleSprites.Length)];
+    }
+}
