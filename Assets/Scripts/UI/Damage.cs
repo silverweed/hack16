a9245d61e3a245292d5ff1becbeb7870ce47fe59
silverweed <silverweed1991@gmail.com>
2016-11-07 16:57:23 +0100

    add damage

---
+++ Assets/Scripts/UI/Damage.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+
+	public float transitionSpeed = 0.05f;
+	public float pulseFrequency = 5f;
+
+	Image image;
+	float level;
+
+	// true: the damage builds up over time
+	// false: the damage decays over time
+	bool Active { get; set; }
+
+	// Use this for initialization
+	void Start() {
+		image = GetComponent<Image>();	
+	}
+	
+	void Update() {
+		// DEBUG
+		if (Input.GetKeyUp(KeyCode.F)) Active = !Active;
+
+		if (Active) {
+			level += transitionSpeed;
+			level += (2 + 7 * level) * transitionSpeed * Mathf.Sin(pulseFrequency * Time.time);
+		} else {
+			if (level == 0f) return;
+			level -= 2f * transitionSpeed;
+		}
+
+
+		level = Mathf.Clamp(level, 0f, 1f);
+		var c = image.color;
+		image.color = new Color(c.r, c.g, c.b, level);
+	}
+}
---
+++ Assets/Scripts/UI/Stressbar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Stressbar : MonoBehaviour {
+
+	Slider slider;
+	Image fillArea, unfillArea;
+
+	void Start() {
+		slider = GetComponent<Slider>();
+		fillArea = transform.FindChild("Fill Area").gameObject.GetComponentInChildren<Image>();
+		unfillArea = transform.FindChild("Unfill Area").gameObject.GetComponentInChildren<Image>();
+	}
+
+	public void Refill(int amt) {
+		slider.value += amt;
+		unfillArea.rectTransform.anchorMax = fillArea.rectTransform.anchorMax;
+	}
+
+	public void Damage(int amt) {
+		slider.value -= amt;
+		StartCoroutine(Unfill());
+	}
+
+	IEnumerator Unfill() {
+		while (unfillArea.rectTransform.anchorMax.x > fillArea.rectTransform.anchorMax.x) {
+			unfillArea.rectTransform.anchorMax = unfillArea.rectTransform.anchorMax - new Vector2(0.01f, 0f);
+			yield return new WaitForSeconds(0.1f);
+		}
+
+		yield return null;
+	}
+}
