695b573c31b6f244e16f738d5079babd630ae79c
silverweed <silverweed1991@gmail.com>
2016-11-07 21:57:30 +0100

    add paparazzo stub

---
+++ Assets/Scripts/NPCs/Paparazzo.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paparazzo : Npc {
+
+	public float speed = 15f;
+	public float patrolRadius = 6f;
+
+	FSM ai;
+	Vector3? target;
+
+	// Use this for initialization
+	void Start() {
+		ai = new FSM(this);
+		StartCoroutine(TakeDecisions());
+	}
+
+	void Update() {
+		if (target.HasValue) 
+			transform.Translate(Time.deltaTime * speed *
+				(transform.position - target.Value).normalized);
+	}
+	
+	IEnumerator TakeDecisions() {
+		while (true) {
+			ai.Update();
+			yield return new WaitForSeconds(.5f);
+		}
+	}
+
+	class FSM {
+		enum State { PATROLLING, MOVING };
+
+		State state = State.PATROLLING;
+		Vector3 target;
+		Paparazzo owner;
+
+		public FSM(Paparazzo owner) {
+			this.owner = owner;
+		}
+
+		public void Update() {
+			switch (state) {
+			case State.PATROLLING:
+				if (PlayerNearby()) {
+					owner.target = target;
+					state = State.MOVING;
+				}
+				break;
+			case State.MOVING:
+				if (Vector3.Distance(owner.transform.position, target) < 0.1f)
+					state = State.PATROLLING;
+				break;
+			}
+			print(state + ", target = " + target);
+		}
+
+		bool PlayerNearby() {
+			var player = GameObject.FindObjectOfType<MovePlayer>();
+			if (Vector3.Distance(owner.transform.position,
+						player.transform.position) < owner.patrolRadius) 
+			{
+				target = player.transform.position + (Vector3)MovePlayer.directionPlayer * 4f;
+				return true;
+			}
+			return false;
+		}
+	}
+}
